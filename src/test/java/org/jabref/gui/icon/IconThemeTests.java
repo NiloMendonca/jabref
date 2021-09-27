5922cff3879599ee1c1285aa9be0db5d53689f6a
Nilo Mendonca <nilojrmendonca@gmail.com>
2021-09-27 16:24:58 -0300

    addition of tests to the class IconTheme

--- src/test/java/org/jabref/gui/icon/IconThemeTests.java
+++ src/test/java/org/jabref/gui/icon/IconThemeTests.java
@@ -3,15 +3,50 @@ package org.jabref.gui.icon;
 import javafx.scene.paint.Color;
 
 import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.Assert.assertThrows;
 
 public class IconThemeTests {
 
+    IconTheme iconTheme = new IconTheme();
+
     @Test
-    void getDefaultGroupColors() {
-        IconTheme iconTheme = new IconTheme();
+    void getDefaultGroupColorsTest() {
         final Color color = Color.web("#8a8a8a");
 
         assertEquals(color, iconTheme.getDefaultGroupColor());
+    }
+
+    @Test
+    void findIconTest() {
+        String iconReturn = "Optional[org.jabref.gui.icon.InternalMaterialDesignIcon@";
+        String icon = iconTheme.findIcon("MOLECULE", Color.web("#8a8a8a")).toString().substring(0, 56);
+        String iconNull = iconTheme.findIcon("", Color.web("#8a8a8a")).toString();
+        String empty = "Optional.empty";
+
+        assertEquals(iconReturn, icon);
+        assertEquals(empty, iconNull);
+        assertThrows(NullPointerException.class, () -> {
+            iconTheme.findIcon("MOLECULE", null);
+        });
+        assertThrows(NullPointerException.class, () -> {
+            iconTheme.findIcon(null, null);
+        });
+    }
+
+    @Test
+    void getIconUrlTest() {
+        String erroUrl = "/external/red.png";
+        String acceptUrl = "/external/JabRef-icon-16.png";
+
+        String urlInvalid = iconTheme.getIconUrl("teste").toString();
+        urlInvalid = urlInvalid.substring(urlInvalid.length() - 17, urlInvalid.length());
+
+        String urlValid = iconTheme.getIconUrl("jabrefIcon16").toString();
+        urlValid = urlValid.substring(urlValid.length() - 28, urlValid.length());
+
+        assertEquals(erroUrl, urlInvalid);
+        assertEquals(acceptUrl, urlValid);
     }
 }
